d59d2d76adf51547f119808432c0caf16f5fe0a2
chikkaRahulgoud <goud4231@gmail.com>
2025-05-04 17:52:03 +0530

    Describe your changes 7

--- src/landing_page/products/Hero.js
+++ src/landing_page/products/Hero.js
@@ -8,7 +8,7 @@ function Hero() {
         <h3 className="text-muted mt-3 fs-4">Sleek, modern and intvitive trading platforms</h3>
         <p className="fs-5 mt-3">
             check out our{" "}
-            <a href=" " style={{textDecoration:"none"}}>
+            <a href="/#" style={{textDecoration:"none"}}>
                 investment offerings{" "}
                 <i className="fa fa-long-arrow-right" aria-hidden="true"></i>
             </a>
