add91f99955653335f8f3e6ce6e55f73fd5bb647
chikkaRahulgoud <goud4231@gmail.com>
2025-05-04 18:01:17 +0530

    Describe your changes 10

--- src/landing_page/Footer.js
+++ src/landing_page/Footer.js
@@ -17,31 +17,31 @@ function Footer() {
             <p>Company</p>
 
             <div className="d-flex flex-column gap-2 ">
-              <a href="" className="text-decoration-none text-muted">
+              <a href="/#" className="text-decoration-none text-muted">
                 About
               </a>
-              <a href="" className="text-decoration-none text-muted">
+              <a href="/#" className="text-decoration-none text-muted">
                 Products
               </a>
-              <a href="" className="text-decoration-none text-muted">
+              <a href="/#" className="text-decoration-none text-muted">
                 Pricing
               </a>
-              <a href="" className="text-decoration-none text-muted">
+              <a href="/#" className="text-decoration-none text-muted">
                 Referral
               </a>
-              <a href="" className="text-decoration-none text-muted">
+              <a href="/#" className="text-decoration-none text-muted">
                 Careers
               </a>
-              <a href="" className="text-decoration-none text-muted">
+              <a href="/#" className="text-decoration-none text-muted">
                 Open source
               </a>
-              <a href="" className="text-decoration-none text-muted">
+              <a href="/#" className="text-decoration-none text-muted">
                 Zerodha.tech
               </a>
-              <a href="" className="text-decoration-none text-muted">
+              <a href="/#" className="text-decoration-none text-muted">
                 Press & media
               </a>
-              <a href="" className="text-decoration-none text-muted">
+              <a href="/#" className="text-decoration-none text-muted">
                 Zerodha cares (CSR)
               </a>
             </div>
@@ -49,32 +49,32 @@ function Footer() {
           <div className="col">
             <p>Support</p>
             <div className="d-flex flex-column gap-2">
-              <a href="" className="text-decoration-none text-muted">
+              <a href="/#" className="text-decoration-none text-muted">
                 Contact us
               </a>
-              <a href="" className="text-decoration-none text-muted">
+              <a href="/#" className="text-decoration-none text-muted">
                 Support portal
               </a>
-              <a href="" className="text-decoration-none text-muted">
+              <a href="/#" className="text-decoration-none text-muted">
                 Z-Connect blog
               </a>
-              <a href="" className="text-decoration-none text-muted">
+              <a href="/#" className="text-decoration-none text-muted">
                 List of charges
               </a>
-              <a href="" className="text-decoration-none text-muted">
+              <a href="/#" className="text-decoration-none text-muted">
                 Downloads & resources
               </a>
-              <a href="" className="text-decoration-none text-muted">
+              <a href="/#" className="text-decoration-none text-muted">
                 Videos
               </a>
-              <a href="" className="text-decoration-none text-muted">
+              <a href="/#" className="text-decoration-none text-muted">
                 Market overview
               </a>
-              <a href="" className="text-decoration-none text-muted">
+              <a href="/#" className="text-decoration-none text-muted">
                 {" "}
                 How to file a complaint?
               </a>
-              <a href="" className="text-decoration-none text-muted">
+              <a href="/#" className="text-decoration-none text-muted">
                 Status of your complaints
               </a>
             </div>
@@ -99,11 +99,11 @@ function Footer() {
             Public School, J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka,
             India. For any complaints pertaining to securities broking please
             write to{" "}
-            <a href="" className="text-decoration-none">
+            <a href="/#" className="text-decoration-none">
               complaints@zerodha.com
             </a>
             , for DP related to{" "}
-            <a href="" className="text-decoration-none">
+            <a href="/#" className="text-decoration-none">
               dp@zerodha.com.
             </a>
             Please ensure you carefully read the Risk Disclosure Document as
@@ -112,13 +112,13 @@ function Footer() {
 
           <p style={{ marginBottom: "10px" }}>
             Procedure to file a complaint on{" "}
-            <a href="" className="text-decoration-none">
+            <a href="/#" className="text-decoration-none">
               SEBI SCORES
             </a>
             : Register on SCORES portal. Mandatory details for filing complaints
             on SCORES: Name, PAN, Address, Mobile Number, E-mail ID. Benefits:
             Effective Communication, Speedy redressal of the grievances <br />
-            <a href="" className="text-decoration-none">
+            <a href="/#" className="text-decoration-none">
               Smart Online Dispute Resolution | Grievances Redressal Mechanism
             </a>
             <br />
@@ -151,7 +151,7 @@ function Footer() {
             bank account. As a business we don't give stock tips, and have not
             authorized anyone to trade on behalf of others. If you find anyone
             claiming to be part of Zerodha and offering such services, please
-            <a href="" className="text-decoration-none">
+            <a href="/#" className="text-decoration-none">
               {" "}
               create a ticket here
             </a>
@@ -161,36 +161,36 @@ function Footer() {
         <div>
           <ul className="d-flex justify-content-center list-unstyled flex-wrap">
             <li className="me-3">
-              <a href="" className="text-decoration-none text-muted">
+              <a href="/#" className="text-decoration-none text-muted">
                 NSE
               </a>
             </li>
             <li className="me-3">
-              <a href="" className="text-decoration-none text-muted">BSE</a>
+              <a href="/#" className="text-decoration-none text-muted">BSE</a>
             </li>
             <li className="me-3">
-              <a href="" className="text-decoration-none text-muted">MCX</a>
+              <a href="/#" className="text-decoration-none text-muted">MCX</a>
             </li>
             <li className="me-3">
-              <a href="" className="text-decoration-none text-muted">Terms & conditions</a>
+              <a href="/#" className="text-decoration-none text-muted">Terms & conditions</a>
             </li>
             <li className="me-3">
-              <a href="" className="text-decoration-none text-muted">Polices & procedures</a>
+              <a href="/#" className="text-decoration-none text-muted">Polices & procedures</a>
             </li>
             <li className="me-3">
-              <a href="" className="text-decoration-none text-muted">Privacy policy</a>
+              <a href="/#" className="text-decoration-none text-muted">Privacy policy</a>
             </li>
             <li className="me-3">
-              <a href="" className="text-decoration-none text-muted">Disclosure</a>
+              <a href="/#" className="text-decoration-none text-muted">Disclosure</a>
             </li>
             <li className="me-3">
-              <a href="" className="text-decoration-none text-muted">For investor's</a>
+              <a href="/#" className="text-decoration-none text-muted">For investor's</a>
             </li>
             <li className="me-3">
-              <a href="" className="text-decoration-none text-muted">attention</a>
+              <a href="/#" className="text-decoration-none text-muted">attention</a>
             </li>
             <li className="me-3">
-              <a href="" className="text-decoration-none text-muted">Investor charter</a>
+              <a href="/#" className="text-decoration-none text-muted">Investor charter</a>
             </li>
           </ul>
         </div>
