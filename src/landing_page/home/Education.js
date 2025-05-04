95ecefc2d3b6839db31989ce76d6e5778b13afec
chikkaRahulgoud <goud4231@gmail.com>
2025-05-04 17:42:25 +0530

    Describe your changes3

--- src/landing_page/home/Education.js
+++ src/landing_page/home/Education.js
@@ -15,10 +15,10 @@ function Education() {
                     <h1 className="mb-3
                     fs-2">Free and open market education</h1>
                     <p className="text-muted">Varsity,the largest online stock market education book in the world covering everything from the basics to advanced trading.</p>
-                    <a href="#" style={{textDecoration:"none"}}>Versity <i className="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                    <a href="/#" style={{textDecoration:"none"}}>Versity <i className="fa fa-long-arrow-right" aria-hidden="true"></i></a>
 
                     <p className="text-muted mt-5">TradingQ&A,the most active trading and investment community in india for all your market related queries.</p>
-                    <a href="#" style={{textDecoration:"none"}}>TradingQ&A <i className="fa fa-long-arrow-right" aria-hidden="true"></i> </a>
+                    <a href="/#" style={{textDecoration:"none"}}>TradingQ&A <i className="fa fa-long-arrow-right" aria-hidden="true"></i> </a>
                 </div>
               </div>
            </div>
